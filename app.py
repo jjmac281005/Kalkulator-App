535de985dfb858d61ebbd7e1f2466b61a9d519ab
jjmac281005 <mac281005@gmail.com>
2026-03-12 19:30:55 +0700

    merubah struktur kode app

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-print("Kalkulator Sederhana")
-
 def penjumlahan(a, b):
     return a + b
 
+if __name__ == "__main__":
+    print("Kalkulator Sederhana")
+    try:
+        angka1 = int(input("Masukkan angka pertama: "))
+        angka2 = int(input("Masukkan angka kedua: "))
 
-try:
-    angka1 = int(input("Masukkan angka pertama: "))
-    angka2 = int(input("Masukkan angka kedua: "))
+        hasil = penjumlahan(angka1, angka2)
+        print("Hasil penjumlahan:", hasil)
 
-    hasil = penjumlahan(angka1, angka2)
-
-    print("Hasil penjumlahan:", hasil)
-
-except ValueError:
-    print("Error: Input harus berupa angka!")
+    except ValueError:
+        print("Error: Input harus berupa angka!")
